958e81aca9a3c187c1134eb11d8dddc248f72dcb
Sivabalan R <sivabalan2005.kpr@gmail.com>
2025-09-08 11:38:51 +0000

    Sync LeetCode submission Runtime - 23 ms (11.78%), Memory - 22.4 MB (47.16%)

---
+++ solutions/0049-group-anagrams/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic={}
+        for i in strs:
+            l=[0]*26
+            for j in i:
+                l[ord(j)-ord('a')]+=1
+            t=tuple(l)
+            if t in dic:
+                dic[t].append(i)
+            else:
+                dic[t]=[i]
+        k=[]
+        for i in dic:
+            k.append(dic[i])
+        return k
