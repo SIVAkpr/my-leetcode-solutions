302847212fff2b22568183a46379898a50cccfff
Sivabalan R <sivabalan2005.kpr@gmail.com>
2025-09-13 08:03:25 +0000

    Sync LeetCode submission Runtime - 59 ms (39.20%), Memory - 33.2 MB (97.11%)

---
+++ solutions/3420-find-occurrences-of-an-element-in-an-array/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
+        index=[]
+        q=queries
+        n=nums
+        ans=[]
+        for i,num in enumerate(n):
+            if num==x:
+                index.append(i)
+        print(index)
+        for query in q:
+            if query <=len(index):
+                ans.append(index[query-1])
+            else:
+                ans.append(-1)
+        return ans
+
