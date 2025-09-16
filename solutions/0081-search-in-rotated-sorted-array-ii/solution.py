b3ff64f19851eb595c4e8a4f785641df96ef4833
Sivabalan R <sivabalan2005.kpr@gmail.com>
2025-09-16 01:28:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (97.33%)

--- solutions/0081-search-in-rotated-sorted-array-ii/solution.py
+++ solutions/0081-search-in-rotated-sorted-array-ii/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         l=len(nums)
-        t=target
-        if t not in nums:
-            return False
-        else:
+        n=nums
+        a=0
+        for i in n:
+            if i==target:
+                a=a+1
+        if a>0:
             return True
+        else:
+            return False
