39da520532b8ca4c240c12b586a62de1b376a08e
Sivabalan R <sivabalan2005.kpr@gmail.com>
2025-09-19 05:08:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (97.88%)

---
+++ solutions/3676-smallest-number-with-all-set-bits/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        lst=[]
+        while(n!=0):
+            a=n%2
+            lst.append(str(a))
+            n=n//2
+        lst0=lst[::-1]
+        
+        for i in range(len(lst)):
+            if lst0[i]=="0":
+                lst0[i]="1"
+        lst1=lst0
+        lst2="".join(lst1)
+        return int(lst2, 2)
+
