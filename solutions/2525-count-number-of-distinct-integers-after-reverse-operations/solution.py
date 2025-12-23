1e6bb8115009032b0fcdc7745c7a3dcd67fb76a3
Sivabalan R <sivabalan2005.kpr@gmail.com>
2025-12-23 10:31:42 +0000

    Sync LeetCode submission Runtime - 145 ms (42.21%), Memory - 43.5 MB (54.61%)

---
+++ solutions/2525-count-number-of-distinct-integers-after-reverse-operations/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDistinctIntegers(self, nums: List[int]) -> int:
+        num=[]
+        for i in nums:
+            rev=int(str(i)[::-1])
+            num.append(rev)
+        print(num)
+        n=num+nums
+        a=set(n)
+        return len(a)
