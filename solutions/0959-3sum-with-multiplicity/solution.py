4b0b0a7aed9802435e12335811b245460bbe2aca
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-01 16:01:16 +0000

    Sync LeetCode submission Runtime - 3052 ms (13.21%), Memory - 19.6 MB (32.57%)

---
+++ solutions/0959-3sum-with-multiplicity/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        l=len(arr)
+        t=target
+        s=0
+        mod=10**9+7
+        frequency_map={}
+        for i in arr:
+            frequency_map[i]=frequency_map.get(i,0)+1
+        
+        for mid_inx,mid_val in enumerate(arr):
+            frequency_map[mid_val]-=1
+            for left in range(mid_inx):
+                left=arr[left]
+                req_val=t-left-arr[mid_inx]
+                s=(s+frequency_map.get(req_val,0))%mod
+        return s
