08ada3a8b29dac90b39cc5a8bd3d97e34ac4c2de
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-09 14:29:16 +0000

    Sync LeetCode submission Runtime - 10 ms (54.92%), Memory - 47.9 MB (8.53%)

---
+++ solutions/2502-sort-the-people/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        String[] n = names;
+        int[] h = heights;
+        int l = n.length;
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < l; i++) {
+            map.put(h[i], n[i]);
+        }
+        Arrays.sort(h);
+        String[] ans = new String[l];
+        for (int i = 0; i < l; i++) {
+            ans[i] = map.get(h[l - i - 1]);
+        }
+        return ans;
+    }
+}
