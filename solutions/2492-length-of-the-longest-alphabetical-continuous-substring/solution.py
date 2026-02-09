50e1ec087893d3db033c6c15d7448124d6731903
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-09 13:40:02 +0000

    Sync LeetCode submission Runtime - 119 ms (86.75%), Memory - 20.2 MB (16.09%)

---
+++ solutions/2492-length-of-the-longest-alphabetical-continuous-substring/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestContinuousSubstring(self, s: str) -> int:
+        l=1
+        res=1
+        if not s:
+            return 0
+        for i in range(1,len(s)):
+            if ord(s[i-1])+1 == ord(s[i]):
+                l=l+1
+                res=max(res,l)
+            else:
+                l=1
+        return res
