5312c2b869e22ea66c243bb73661d259b2595c83
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-10 11:35:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.6 MB (5.29%)

---
+++ solutions/0020-valid-parentheses/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in s:
+            if i in "({[":
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                top=stack.pop()
+                if i==')' and top!='(':
+                    return False
+                elif i=='}' and top!='{':
+                    return False
+                elif i==']' and top!='[':
+                    return False
+        return not stack
