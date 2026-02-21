da13d8c53827061e223492cc965c18b4d491e711
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-21 15:22:20 +0000

    Sync LeetCode submission Runtime - 1 ms (84.07%), Memory - 42.4 MB (28.61%)

---
+++ solutions/2288-count-operations-to-obtain-zero/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int ans=0;
+        if(num1==0||num2==0){
+            return 0;
+        }
+        while(true){
+            if(num1<num2){
+                num2-=num1;
+                ans++;
+            }
+            else if(num1>num2){
+                num1-=num2;
+                ans++;
+            }
+            else if(num1==num2){
+                num1-=num2;
+                ans++;
+            }
+            if(num1==0||num2==0){
+                break;
+            }
+        }
+        return ans;
+    }
+}
