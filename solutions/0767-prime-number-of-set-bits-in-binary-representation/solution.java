e88b3e5a7eb89cd7c897992ecf63a15febb8f5e1
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-21 15:13:47 +0000

    Sync LeetCode submission Runtime - 304 ms (5.04%), Memory - 46.8 MB (5.67%)

---
+++ solutions/0767-prime-number-of-set-bits-in-binary-representation/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        Integer ans=0;
+        for(int i=left;i<=right;i++){
+            int c=0;
+            String a=dec2bin(i);
+            for(int j=0;j<a.length();j++){
+                if(a.charAt(j)=='1'){
+                    c++;
+                }
+            }
+            if(c==2||c==3||c==5||c==7||c==11||c==13||c==17||c==19||c==23||c==27||c==31||c==37){
+                ans++;
+            }
+        }
+        return ans;      
+    }
+    public String dec2bin(int n){
+        String a="";
+        while(n>0){
+            if((n%2)==0){
+                a="0"+a;
+            }
+            else{
+                a="1"+a;
+            }
+            n=n/2;
+        }
+        return a;
+    }
+}
