5939cad9c81be9e7a3a3008a135838aa3401c36c
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-24 10:06:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (64.45%)

---
+++ solutions/0405-convert-a-number-to-hexadecimal/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num==0:
+            return "0"
+        dit={10:'a',11:'b',12:'c',13:'d',14:'e',15:'f'}
+        ans=""
+        n=num & 0xFFFFFFFF
+        # print(n)
+        while n>0:
+            ad=n%16
+            if ad >9:
+                ad=dit[ad]
+            ans=str(ad)+ans
+            n//=16
+        return ans
