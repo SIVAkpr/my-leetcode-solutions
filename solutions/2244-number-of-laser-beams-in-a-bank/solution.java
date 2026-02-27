b89ea5a118e0769e85dbaef0cf26bc858914eb01
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-27 06:28:48 +0000

    Sync LeetCode submission Runtime - 11 ms (86.93%), Memory - 47.6 MB (87.25%)

---
+++ solutions/2244-number-of-laser-beams-in-a-bank/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int[] arr=new int[bank.length+2];
+        int k=0;
+        int a=0;
+        for(String i:bank){
+            int cnt=0;
+            for(char j:i.toCharArray()){
+                if(j=='1'){
+                    cnt+=1;
+                }
+            }
+            if(cnt!=0){
+                arr[k++]=cnt;
+                a=a+1;
+            }
+        }
+        int mul=0;
+        for(int i=1;i<a;i++){
+            mul+=(arr[i]*arr[i-1]);
+        }
+        return mul;
+    }
+}
