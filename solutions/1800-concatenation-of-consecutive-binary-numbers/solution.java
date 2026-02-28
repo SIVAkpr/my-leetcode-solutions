7443138e4ca8e568033a4353ec92730905be4227
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-02-28 09:32:36 +0000

    Sync LeetCode submission Runtime - 24 ms (98.29%), Memory - 42.1 MB (88.97%)

---
+++ solutions/1800-concatenation-of-consecutive-binary-numbers/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long mod=1000000007L;
+        int len=0;
+        long ans=0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0){
+                len++;
+            }
+            ans=((ans<<len)+i)%mod;
+        }
+        return (int)ans;
+    }
+}
