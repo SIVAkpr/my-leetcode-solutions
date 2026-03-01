2b985baf4ee131f246ffd9a02c809bb5735c87be
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-01 06:02:06 +0000

    Sync LeetCode submission Runtime - 43 ms (79.35%), Memory - 31.7 MB (74.64%)

---
+++ solutions/0951-partition-array-into-disjoint-intervals/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionDisjoint(self, nums: List[int]) -> int:
+        n=nums
+        l_max=n[0]
+        c_max=n[0]
+        partition=0
+        for i in range(1,len(n)):
+            c_max=max(c_max,n[i])
+            if n[i]<l_max:
+                l_max=c_max
+                partition =i
+        return partition+1
