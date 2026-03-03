fcfe3e658705f8ab78e721342cadc7ea39b224a6
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-03 14:43:33 +0000

    Sync LeetCode submission Runtime - 19 ms (36.05%), Memory - 19.8 MB (56.93%)

---
+++ solutions/1031-add-to-array-form-of-integer/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        carry=0
+        rst=[]
+        index=len(num)-1
+        while k>0 or index>=0:
+            if index>=0:
+                k+=num[index]
+            carry,digit=divmod(k,10)
+            rst.append(digit)
+            k=carry
+            index-=1
+        return rst[::-1]
