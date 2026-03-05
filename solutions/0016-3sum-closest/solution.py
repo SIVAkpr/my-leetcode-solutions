b205a03666553cf82c2cb30ce8b8876dd1b41b1c
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-05 14:34:04 +0000

    Sync LeetCode submission Runtime - 403 ms (25.02%), Memory - 19.4 MB (46.00%)

---
+++ solutions/0016-3sum-closest/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        t=target
+        n=sorted(nums)
+        clo_sum=sys.maxsize
+        l=len(n)
+        for i in range(l-2):
+            left,right=i+1,l-1
+            while left<right:
+                cur_sum=n[i]+n[left]+n[right]
+                if abs(cur_sum-t)<abs(clo_sum-t):
+                    clo_sum=cur_sum
+                if cur_sum==t:
+                    return cur_sum
+                elif cur_sum<t:
+                    left+=1
+                else:
+                    right-=1
+        return clo_sum
