9215c30714163dcf5ee7e3f27e080670a63acaa4
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-05 02:09:38 +0000

    Sync LeetCode submission Runtime - 8 ms (55.03%), Memory - 19.2 MB (83.83%)

---
+++ solutions/1884-minimum-changes-to-make-alternating-binary-string/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        l=len(s)
+        cnt=0
+        s=list(s)
+        for i in range(1,len(s)):
+            if s[i]==s[i-1]:
+                cnt+=1
+                if s[i]=='0':
+                    s[i]='1'
+                else:
+                    s[i]='0'
+        return min(cnt,l-cnt)
