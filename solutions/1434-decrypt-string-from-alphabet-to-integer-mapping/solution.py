e035beec606e57de11a1e4e5151cec1aae259c41
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-08 08:10:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (86.47%)

---
+++ solutions/1434-decrypt-string-from-alphabet-to-integer-mapping/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        ans=""
+        l=len(s)
+        i=0
+        while i<l:
+            if i+2<len(s) and s[i+2]=='#':
+                ans+=chr(int(s[i:i+2])+ord('a')-1)
+                i+=3
+            else:
+                ans+=chr(int(s[i])+ord('a')-1)
+                i+=1
+        return ans
