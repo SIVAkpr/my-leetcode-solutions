dcd43c65727499341ac63318d3b25fc9b94ea624
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:32 +0530

    Create 2524-LargestPositiveIntegerThatExistsWithItsNegative

---
+++ 2524-LargestPositiveIntegerThatExistsWithItsNegative/2524-LargestPositiveIntegerThatExistsWithItsNegative.py
@@ -0,0 +1,25 @@
+# Last updated: 3/21/2026, 3:39:26 PM
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        q=0
+        rst=[]
+        a=0
+        print(len(nums))
+        while(1):
+            maxx=nums[q]
+            for i in range(q+1,len(nums)):
+                if(maxx==(-1*nums[i])):
+                    rst.append(maxx)
+            a=a+1
+            q=q+1
+            if(a==len(nums)):
+                break
+        if(rst):
+            for i in range(len(rst)):
+                if(rst[i]<0):
+                    rst[i]=-1*rst[i]
+            return max(rst)
+        else:
+            return -1
+
+            
