f7a9798f3f354ab90dd35c56880bf20296e00fcf
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:40:21 +0530

    Create 1064-SmallestIntegerDivisibleByK

---
+++ 1064-SmallestIntegerDivisibleByK/1064-SmallestIntegerDivisibleByK.java
@@ -0,0 +1,14 @@
+// Last updated: 3/21/2026, 3:40:14 PM
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        int n=0;
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+        for(int i=1;i<=k;i++){
+            n=((n*10)+1)%k;
+            if(n==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
