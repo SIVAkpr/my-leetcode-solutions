60f733b09e3bd843baceb52cb6c07d7764b6ad7d
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:41:14 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,17 @@
+# Last updated: 3/21/2026, 3:41:08 PM
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        size=len(nums)
+        a=nums
+        q=[]
+        def permu(a,size):
+            if size==1:
+                q.append(tuple(a))
+            for i in range(size):
+                permu(a,size-1)
+                if size%2==1:
+                    a[0],a[size-1]=a[size-1],a[0]
+                else:
+                    a[i],a[size-1]=a[size-1],a[i]
+        permu(a,size)
+        return q
