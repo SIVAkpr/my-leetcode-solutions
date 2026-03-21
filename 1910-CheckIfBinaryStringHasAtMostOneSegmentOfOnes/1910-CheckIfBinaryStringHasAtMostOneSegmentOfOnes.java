bc84e15a22b217d96078761412fec8fe554ca6d4
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:47 +0530

    Create 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes/1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes.java
@@ -0,0 +1,11 @@
+// Last updated: 3/21/2026, 3:39:41 PM
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        if(s.contains("01")){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
