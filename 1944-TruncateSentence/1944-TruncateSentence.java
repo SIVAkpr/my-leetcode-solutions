35ae8cc077ebab19a77038cd6839fc1e6981f4f7
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:46 +0530

    Create 1944-TruncateSentence

---
+++ 1944-TruncateSentence/1944-TruncateSentence.java
@@ -0,0 +1,18 @@
+// Last updated: 3/21/2026, 3:39:40 PM
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] st=s.split(" ");
+        String res="";
+        String a=" ";
+        for(int i=0;i<k;i++){
+            if(i!=(k-1)){
+                res+=st[i]+a;
+            }
+            else{
+                res+=st[i];
+            }
+        }
+        //System.out.println(st);
+        return res;
+    }
+}
