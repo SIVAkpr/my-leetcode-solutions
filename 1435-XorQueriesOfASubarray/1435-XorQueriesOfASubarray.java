9a3b95ed11c9b994a342ba5706ac7e6202675fbd
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:40:11 +0530

    Create 1435-XorQueriesOfASubarray

---
+++ 1435-XorQueriesOfASubarray/1435-XorQueriesOfASubarray.java
@@ -0,0 +1,33 @@
+// Last updated: 3/21/2026, 3:40:05 PM
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] array=new int[queries.length];
+        // int j=0;
+        // for(int[] i:queries){
+        //     int a=0;
+        //     if(i[0]==i[1]){
+        //         array[j]=arr[i[0]];
+        //         j++;
+        //         continue;
+        //     }
+        //     for(int k=i[0];k<=i[1];k++){
+        //         a^=arr[k];
+        //     }
+        //     array[j++]=a;
+        // }
+        // return array;
+        int[] xors = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; ++i)
+        xors[i + 1] = xors[i] ^ arr[i];
+
+        int i = 0;
+        for (int[] query : queries) {
+        final int left = query[0];
+        final int right = query[1];
+        array[i++] = xors[left] ^ xors[right + 1];
+        }
+
+        return array;
+    }
+}
