e7f9c2ad132f61a2cde99b8611c905b900f4b351
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:57 +0530

    Create 1667-FindKthBitInNthBinaryString

---
+++ 1667-FindKthBitInNthBinaryString/1667-FindKthBitInNthBinaryString.py
@@ -0,0 +1,12 @@
+# Last updated: 3/21/2026, 3:39:51 PM
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        def reverse_invert(a):
+            ans=a[::-1]
+            ans2="".join(["1" if char == "0" else "0" for char in a])
+            return ans2[::-1]
+        dit={1:"0"}
+        for i in range(2,n+1):
+            s_n=dit[i-1] + "1" + reverse_invert(dit[i-1])
+            dit[i]=s_n
+        return dit[n][k-1]
