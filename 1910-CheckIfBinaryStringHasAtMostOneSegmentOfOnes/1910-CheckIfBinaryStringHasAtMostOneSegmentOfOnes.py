32c9eac549bcbde14e9d312e0c4a9f44ea97ad8c
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:52 +0530

    Create 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes/1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes.py
@@ -0,0 +1,7 @@
+# Last updated: 3/21/2026, 3:39:45 PM
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        if '01'in s:
+            return False
+        else:
+            return True
