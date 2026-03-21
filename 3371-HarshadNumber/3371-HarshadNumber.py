a4715e5c9a492eba161607f673ccc8c135bff887
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:25 +0530

    Create 3371-HarshadNumber

---
+++ 3371-HarshadNumber/3371-HarshadNumber.py
@@ -0,0 +1,13 @@
+# Last updated: 3/21/2026, 3:39:19 PM
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        n=0
+        s=x
+        while(x!=0):
+            a=x%10
+            n=n+a
+            x=x//10
+        if((s%n)==0):
+            return n
+        else:
+            return -1
