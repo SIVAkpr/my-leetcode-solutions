9ae87ce085041263680b167ff05e7c9748d5fa74
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:40:17 +0530

    Create 1210-MeanOfArrayAfterRemovingSomeElements

---
+++ 1210-MeanOfArrayAfterRemovingSomeElements/1210-MeanOfArrayAfterRemovingSomeElements.py
@@ -0,0 +1,13 @@
+# Last updated: 3/21/2026, 3:40:11 PM
+class Solution(object):
+    def trimMean(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: float
+        """
+        arr.sort()
+        n=len(arr)
+        N=int(n*0.05)
+        trim=arr[N:n-N]
+        return sum(trim)/float(len(trim))
+        
