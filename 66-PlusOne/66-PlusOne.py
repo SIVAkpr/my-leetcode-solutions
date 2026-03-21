f371982182e9d215c72cef2754768973323d7e09
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:41:10 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,18 @@
+# Last updated: 3/21/2026, 3:41:04 PM
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        l=len(digits)
+        a=""
+        for i in range(0,l):
+            a=a+str(digits[i])
+        b=int(a)        
+        c1=b+1
+        c=str(c1)
+        d=[]
+        print(c)
+        for i in c:
+            print(i)
+            e=int(i)
+            d.append(e)
+        print(d)
+        return d
