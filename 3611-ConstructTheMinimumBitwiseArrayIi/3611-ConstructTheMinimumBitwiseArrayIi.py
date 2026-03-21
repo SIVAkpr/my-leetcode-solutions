5840c1e1db79eb833f56dfe3d4ffaa7e6f1249e0
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:21 +0530

    Create 3611-ConstructTheMinimumBitwiseArrayIi

---
+++ 3611-ConstructTheMinimumBitwiseArrayIi/3611-ConstructTheMinimumBitwiseArrayIi.py
@@ -0,0 +1,15 @@
+# Last updated: 3/21/2026, 3:39:14 PM
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        rst=[]
+        for i in nums:
+            if i==2:
+                rst.append(-1)
+            else:
+                for bit_pos in range(1,32):
+                    a=i>>bit_pos
+                    # if(a | (a+1))==i:
+                    if (a&1)^1:
+                        rst.append(i ^ (1<<bit_pos-1))
+                        break
+        return rst
