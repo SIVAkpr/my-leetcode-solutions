21e3359ddeda5858281396da8a437b363cf12525
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:39:14 +0530

    Create 4003-LongestFibonacciSubarray

---
+++ 4003-LongestFibonacciSubarray/4003-LongestFibonacciSubarray.py
@@ -0,0 +1,17 @@
+# Last updated: 3/21/2026, 3:39:08 PM
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        n=nums
+        l=len(n)
+        if l<3:
+            return l
+        m_len=2
+        c_len=2
+        for i in range(2,l):
+            if (n[i]==(n[i-1]+n[i-2])):
+                c_len=c_len+1
+                m_len=max(m_len,c_len)
+            else:
+                c_len=2
+        return m_len
+                
