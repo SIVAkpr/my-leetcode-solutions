a456c785b789bcc57c704bf0b374d196d0531f30
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:40:03 +0530

    Create 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK

---
+++ 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK/1557-CheckIfAStringContainsAllBinaryCodesOfSizeK.java
@@ -0,0 +1,16 @@
+// Last updated: 3/21/2026, 3:39:57 PM
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        if(s.length()< Math.pow(2,k)){
+            return false;
+        }
+        Set<String> myset = new HashSet<>();
+        for(int i=0;i<s.length()-k+1;i++){
+            myset.add(s.substring(i,i+k));
+            if(myset.size()==Math.pow(2,k)){
+                return true;
+            }
+        }
+        return myset.size()==Math.pow(2,k);
+    }
+}
