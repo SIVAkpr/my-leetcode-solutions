56285522b4bbaefb208396b1b3979823a8fe590e
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:41:12 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,15 @@
+# Last updated: 3/21/2026, 3:41:06 PM
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        rst=1
+        if n==0:
+            return 1
+        if(n<0):
+            n=-n
+            x=1/x
+        while n>0:
+            if n%2==1:
+                rst*=x
+            x*=x
+            n//=2
+        return rst
