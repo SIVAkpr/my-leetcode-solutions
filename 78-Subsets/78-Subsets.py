1c89513e11399f16dd171a2a1d8a1bef6c4b239b
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:41:02 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.py
@@ -0,0 +1,11 @@
+# Last updated: 3/21/2026, 3:40:56 PM
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        lst=[[]]
+        for num in nums:
+            lst+=[i+[num] for i in lst]
+        return lst
+        # for i in range(len(nums)+1):
+        #     for j in range(i+1,len(nums)+1):
+        #         lst.append(nums[i:j])
+        # return sorted(lst,key=sum)
