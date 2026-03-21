df32edb7738d240e6c955f709dd04e0b3c014350
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:41:37 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -0,0 +1,32 @@
+# Last updated: 3/21/2026, 3:41:30 PM
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        row=numRows
+        mat=[[] for _ in range(row+1)]
+        l=len(s)
+        a=0
+        r=0
+        while(l!=0):
+            l=l-1
+            if(r<row):
+                mat[r].append(s[a])
+                a=a+1
+                r=r+1
+            elif(r==(row)):
+                r=r-1
+                if(r==0):
+                    r=r+1
+                while(r!=0):
+                    r=r-1
+                    if(a==len(s)):
+                        break
+                    mat[r].append(s[a])
+                    a=a+1
+                r=r+1
+            if(a==len(s)):
+                break
+        rst=""
+        for i in mat:
+            for j in i:
+                rst=rst+j
+        return rst
