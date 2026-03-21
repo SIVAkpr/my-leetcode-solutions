03fb4cdfcad27e3c89c27912f2fb65f62777cbca
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-03-21 15:40:30 +0530

    Create 886-ScoreOfParentheses

---
+++ 886-ScoreOfParentheses/886-ScoreOfParentheses.py
@@ -0,0 +1,11 @@
+# Last updated: 3/21/2026, 3:40:23 PM
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = [0]
+        for char in s:
+            if char == '(':
+                stack.append(0)
+            else:
+                v = stack.pop()
+                stack[-1] += max(2 * v, 1)
+        return stack.pop()
