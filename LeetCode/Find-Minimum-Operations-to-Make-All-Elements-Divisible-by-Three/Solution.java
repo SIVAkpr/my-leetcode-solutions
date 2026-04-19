133e657a1389e68bc3199734be07444b2f39c2be
Sivabalan R <sivabalan2005.kpr@gmail.com>
2026-04-19 21:16:03 +0530

    Add solution for Find Minimum Operations to Make All Elements Divisible by Three

--- LeetCode/Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three/Solution.java
+++ LeetCode/Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three/Solution.java
@@ -2,8 +2,9 @@
 2    public int minimumOperations(int[] nums) {
 3        int cnt=0;
 4        for(int i:nums){
-5            cnt+=Math.min(i%3,3-(i%3));
-6        }
-7        return cnt;
-8    }
-9}
+5            // cnt+=Math.min(i%3,3-(i%3));
+6            if(i%3!=0) cnt++;
+7        }
+8        return cnt;
+9    }
+10}
